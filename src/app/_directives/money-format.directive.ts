1d99202ea58bd03c0b93812603117a1efab2b4f8
Ha Tran <hatq@viettelsoftware.com>
2026-03-16 17:04:01 +0700

    input numberic

--- src/app/_directives/money-format.directive.ts
+++ src/app/_directives/money-format.directive.ts
@@ -1,16 +1,16 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
+import { NgControl } from '@angular/forms';
 
 @Directive({
   selector: '[appMoneyFormat]'
 })
 export class MoneyFormatDirective {
-
-  constructor(private el: ElementRef) {}
-
-  // chặn ký tự không phải số
+  constructor(
+    private el: ElementRef,
+    private control: NgControl
+  ) {}
   @HostListener('keydown', ['$event'])
   onKeyDown(event: KeyboardEvent) {
-
     const allowedKeys = [
       'Backspace',
       'Delete',
@@ -18,30 +18,26 @@ export class MoneyFormatDirective {
       'ArrowRight',
       'Tab'
     ];
-
-    if (allowedKeys.includes(event.key)) {
-      return;
-    }
-
+    if (allowedKeys.includes(event.key)) return;
     if (!/^[0-9]$/.test(event.key)) {
       event.preventDefault();
     }
   }
-
-  // format khi nhập
-  @HostListener('input')
-  onInput() {
-
-    let value = this.el.nativeElement.value;
-
-    if (!value) return;
-
-    value = value.replace(/\./g, '');
-    value = value.replace(/[^0-9]/g, '');
-
-    const formatted = value.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
-
-    this.el.nativeElement.value = formatted;
+@HostListener('input')
+onInput() {
+  let value = this.el.nativeElement.value;
+  if (!value) {
+    this.control.control?.setValue(null, { emitEvent: false });
+    return;
   }
+  let rawValue = value.replace(/\./g, '').replace(/[^0-9]/g, '');
+  rawValue = rawValue.replace(/^0+/, '');
 
+  if (rawValue === '') {
+    rawValue = '0';
+  }
+  this.control.control?.setValue(Number(rawValue), { emitEvent: false });
+  const formatted = rawValue.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+  this.el.nativeElement.value = formatted;
+}
 }
